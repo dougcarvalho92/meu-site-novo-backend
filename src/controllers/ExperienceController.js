199672bbc3b0e76bf1384d643f9915ed1ea10475
Douglas Carvalho <dscarvalho199@gmail.com>
2021-07-22 10:02:11 -0300

    order by dat

--- src/controllers/ExperienceController.js
+++ src/controllers/ExperienceController.js
@@ -4,10 +4,7 @@ const Experiences = mongoose.model('Experiences');
 module.exports = {
   async index(req, res) {
     const { page = 1 } = req.query;
-    const experiences = await Experiences.find()
-      .sort({ endsAt: -1 })
-      .paginate({}, { page, limit: 10 })
-      .then();
+    const experiences = await Experiences.find().sort({ endsAt: -1 }).limit(10);
 
     return res.json(experiences);
   },
